be720a06e64caf331ef2885837adec8cd67244f3
rutttaba <42845851+rutttaba@users.noreply.github.com>
2019-08-30 15:31:40 +0300

    fixes to js file

--- index.js
+++ index.js
@@ -1,22 +1,14 @@
 
 $(function () {
 
-    $('button[type="submit"]').click(function(event) {
-        var newItem = $("input[type='text']").val();
-        $(this).val("");
+    $('#js-shopping-list-form').submit(function (event) {
+        event.preventDefault();
+        let newItem = $("input[type='text']").val();
+        $("#shopping-list-entry").val("");
         $("ul").append('<li><span class="shopping-item">' +
             newItem +
             '</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button><button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>');
-    });
-
-    $('input[type="text"]').keypress(function(event) {
-        if (event.which === 13) {
-            $(this).val("");
-            $("ul").append('<li><span class="shopping-item">' +
-                newItem +
-                '</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button><button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>');
-        }
-    });
+    });    
 
     $("ul").on("click", ".shopping-item-toggle", function() {
         $(this).parents("li").find(".shopping-item").toggleClass("shopping-item__checked");
